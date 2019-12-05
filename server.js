79e617f80b55cb6bb2f93247f7aa3e4bd72fb568
ruchigarg-bib <ruchi.garg_ext@michelin.com>
2019-12-05 19:59:50 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname+"dist/index.html";
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(process.env.PORT || 8080);
+app.use('/', express.static('dist'));
+app.listen(process.env.PORT || 3000, function () {console.log('Example app listening on port 3000!');});
